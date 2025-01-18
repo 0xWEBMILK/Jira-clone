50672493298978d1de43ae7fb6d71ed3dcf5d3ff
D. Adamson <0xwebmilk@gmail.com>
2025-01-18 22:45:14 +0300

    Added basic hasher
    - Added hasher interface
    - Added basic JWT encoder & decoder

---
+++ app/auth/hashing.py
@@ -0,0 +1,31 @@
+from app.models.models import User
+from datetime import datetime, timedelta, UTC
+from jose import jwt
+
+class HashingInterface:
+    def __init__(self, hasher):
+        self.hasher = hasher
+
+    def encode(self, user: User) -> str:
+        return self.hasher.encode(user)
+
+    def decode(self, token) -> str:
+        return self.hasher.decode(token)
+
+
+class JWTHasher:
+    def __init__(self, key, alg = 'HS256'):
+        self.key = key
+        self.alg = alg
+
+    def encode(self, user: User):
+        to_encode = user.model_dump()
+        expire_date = datetime.now(UTC) + timedelta(minutes=30)
+
+        to_encode['exp'] = expire_date
+
+        encoded = jwt.encode(to_encode, self.key, self.alg)
+        return encoded
+
+    def decode(self, token):
+        return jwt.decode(token, self.key, self.alg)
