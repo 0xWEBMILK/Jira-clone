81bf380681dea38dace5ddce61f966ff129c2dfb
D. Adamson <0xwebmilk@gmail.com>
2025-01-19 22:31:41 +0300

    Added Update method
    - Added update method to Task repository
    - Added update method to User repository

--- jira_clone/app/repositories/task_repository.py
+++ jira_clone/app/repositories/task_repository.py
@@ -8,6 +8,10 @@ class TaskRepository:
     def remove_task(self, token: str):
         self.session.remove('tasks', token)
 
+    def update_task(self, old_token: str, new_token: str):
+        if self.is_task_exist(old_token):
+            self.session.update('tasks', old_token, new_token)
+
     def is_task_exist(self, token: str):
         for item in self.session.all()['tokens']:
             if item == token:
--- jira_clone/app/repositories/user_repository.py
+++ jira_clone/app/repositories/user_repository.py
@@ -8,7 +8,11 @@ class UserRepository:
     def remove_user(self, token: str):
         self.session.remove('users', token)
 
-    def is_user_exist(self, token: str):
+    def update_user(self, old_token: str, new_token: str):
+        if self.get_user_by_token(old_token):
+            self.session.update('users', old_token, new_token)
+
+    def get_user_by_token(self, token: str):
         for item in self.session.all()['users']:
             if item == token:
                 return item
