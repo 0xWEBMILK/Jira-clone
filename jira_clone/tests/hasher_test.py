3e777199768a08a5904e6704edefd5cfd57fbb0a
D. Adamson <0xwebmilk@gmail.com>
2025-01-19 19:05:33 +0300

    Some hasher test refactor
    - Added task encoding test
    - Added task decoding test

--- jira_clone/tests/hasher_test.py
+++ jira_clone/tests/hasher_test.py
@@ -3,14 +3,17 @@ import re
 
 
 from jira_clone.app.auth.hashing import HasherInterface, JWTHasher
-from jira_clone.app.models.models import User
+from jira_clone.app.models.models import User, Task
 
-
-def test_hasher_encode():
+# User
+def test_user_encode():
     test_user = User(
+        first_name='John',
+        last_name='Doe',
+
         username="super_username",
         password="super_password",
-        email="super_email@super_domain.zone",
+        email="superemail@superdomain.zone",
     )
 
     jwt_hasher = JWTHasher(key='super_key', alg='HS256')
@@ -20,17 +23,56 @@ def test_hasher_encode():
 
     assert re.fullmatch(r"^[A-Za-z0-9-_]+(.[A-Za-z0-9-_]+){2}$", encoded)
 
-def test_hasher_decode():
+def test_user_decode():
     test_user = User(
+        first_name='John',
+        last_name='Doe',
+
         username="super_username",
         password="super_password",
-        email="super_email@super_domain.zone",
+        email="superemail@superdomain.zone",
     )
 
     jwt_hasher = JWTHasher(key='super_key', alg='HS256')
     jwt_hasher_interface = HasherInterface(jwt_hasher)
 
     encoded = jwt_hasher_interface.encode(test_user)
-    decoded = jwt_hasher_interface.decode(encoded)
+    decoded = User(**jwt_hasher_interface.decode(encoded))
 
-    assert isinstance(decoded, User)
+    assert decoded == test_user
+
+# Task
+def test_task_encode():
+    test_task = Task(
+        title="Test Task Title",
+        description="Test Description",
+
+        creator="John Doe",
+        performers=["Test Performer One", "Test Performer Two"],
+        tags=["Test Tag One", "Test Tag Two"],
+    )
+
+    jwt_hasher = JWTHasher(key='super_key', alg='HS256')
+    jwt_hasher_interface = HasherInterface(jwt_hasher)
+
+    encoded = jwt_hasher_interface.encode(test_task)
+
+    assert re.fullmatch(r"^[A-Za-z0-9-_]+(.[A-Za-z0-9-_]+){2}$", encoded)
+
+def test_task_decode():
+    test_task = Task(
+        title="Test Task Title",
+        description="Test Description",
+
+        creator="John Doe",
+        performers=["Test Performer One", "Test Performer Two"],
+        tags=["Test Tag One", "Test Tag Two"],
+    )
+
+    jwt_hasher = JWTHasher(key='super_key', alg='HS256')
+    jwt_hasher_interface = HasherInterface(jwt_hasher)
+
+    encoded = jwt_hasher_interface.encode(test_task)
+    decoded = Task(**jwt_hasher_interface.decode(encoded))
+
+    assert decoded == test_task
