9a8079033b75841d96335e59d5c90ff28cf26d5a
D. Adamson <0xwebmilk@gmail.com>
2025-01-22 23:39:16 +0300

    Some refactoring

--- jira_clone/app/controllers/task_controller.py
+++ jira_clone/app/controllers/task_controller.py
@@ -8,22 +8,21 @@ from ..schemas import TaskSchema
 
 task_router = APIRouter(prefix='/tasks', tags=['tasks'])
 
-
 @task_router.get('/')
-def create_task(task_schema: TaskSchema, session = Depends(get_session_stub), hasher = Depends(get_hasher_stub)):
+def get_all_tasks(session=Depends(get_session_stub), hasher=Depends(get_hasher_stub)):
     task_repository = TaskRepository(session)
     task_interactor = TaskInteractor(task_repository, hasher)
 
-    result = task_interactor.create_task(task_schema)
+    result = task_interactor.get_all()
 
-    return result
+    return result if result is not None else HTTPException(status_code=404)
 
 @task_router.get('/{task_token}')
 def get_task_by_token(task_token: str, session = Depends(get_session_stub), hasher = Depends(get_hasher_stub)):
     task_repository = TaskRepository(session)
     task_interactor = TaskInteractor(task_repository, hasher)
 
-    result = task_interactor.get_task_by_token(task_token)
+    result = task_interactor.get_by_token(task_token)
 
     return result if result is not None else HTTPException(status_code=404)
 
@@ -32,16 +31,16 @@ def create_task(task_schema: TaskSchema, session = Depends(get_session_stub), ha
     task_repository = TaskRepository(session)
     task_interactor = TaskInteractor(task_repository, hasher)
 
-    result = task_interactor.create_task(task_schema)
+    result = task_interactor.create(task_schema)
 
     return result if result is not None else HTTPException(status_code=404)
 
 @task_router.put('/')
-def update_task(old_task_schema: TaskSchema, new_task_schema: TaskSchema, session = Depends(get_session_stub), hasher = Depends(get_hasher_stub)):
+def update_task(old_schema: TaskSchema, new_schema: TaskSchema, session = Depends(get_session_stub), hasher = Depends(get_hasher_stub)):
     task_repository = TaskRepository(session)
     task_interactor = TaskInteractor(task_repository, hasher)
 
-    result = task_interactor.update_task(old_task_schema, new_task_schema)
+    result = task_interactor.update(old_schema, new_schema)
 
     return result if result is not None else HTTPException(status_code=404)
 
@@ -50,6 +49,6 @@ def delete_task(task_schema: TaskSchema, session = Depends(get_session_stub), ha
     task_repository = TaskRepository(session)
     task_interactor = TaskInteractor(task_repository, hasher)
 
-    result = task_interactor.remove_task(task_schema)
+    result = task_interactor.remove(task_schema)
 
     return result if result is not None else HTTPException(status_code=404)
